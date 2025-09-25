3c46de08c5d84cb8ee97109277ea60aa1100ebd5
Zitcheron <90611998+AdhaTito@users.noreply.github.com>
2025-09-25 21:13:32 +0700

    Update import Navbar from "./components/Navbar"
    Sementara

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // import { Routes, Route } from "react-router-dom";
 // import { Navbar } from "flowbite-react";
-import Navbar from "./Components/Navbar";
+import Navbar from "./components/Navbar";
 import Hero from "./Sections/Hero";
 import About from "./Sections/About";
 import Portofolio from "./Sections/Porrtofolio";
