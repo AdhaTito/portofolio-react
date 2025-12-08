816071894fa365957648193cddb17497cb10f639
Zitcheron <adhamastito55@gmail.com>
2025-12-08 07:34:06 +0700

    perbaikan

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import { Routes, Route } from "react-router-dom";
 // import { Navbar } from "flowbite-react";
-import Navbar from "./components/Navbar";
+// import { Sertifikat } from "flowbite-react";
+import Navbar from "./Components/Navbar";
 import Hero from "./Sections/Hero";
 import About from "./Sections/About";
 import Portofolio from "./Sections/Porrtofolio";
